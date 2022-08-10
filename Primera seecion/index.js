4070d1ac554d7a5d94668e3a0de7ff49896272ab
Jonathan Rincon <Jonathanandres080@gmail.com>
2022-08-10 11:48:01 -0500

    Primera seccion terminada

---
+++ Primera seecion/index.js
@@ -0,0 +1,11 @@
+//Primer mensaje de mensaje
+console.log("Por aqui empieza el proyecto");
+
+/*
+Comentario de varias
+lineas
+
+*/
+
+//Ultima linea de comando 
+console.log("ultimo mensaje, despues se le da npm start en el terminal ");
