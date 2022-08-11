39ee1a1c8af5d7aba7db6be1a61f86b82a836682
Jonathan Rincon <Jonathanandres080@gmail.com>
2022-08-11 02:13:05 -0500

    commit v2 tercera seccion

--- TerceraSeccion/factorial-break.js
+++ TerceraSeccion/factorial-break.js
@@ -1,8 +1,13 @@
 let value = 10;
-var i=1;
+var i = 1;
 
-do{
-    value = value*i;
+do {
+  if (i <= 10) {
+    value = value * i;
     console.log(value);
     i++;
-}while(i <= 10)
+  }else if (i > 10) {
+    break;
+  }
+
+} while (true);
