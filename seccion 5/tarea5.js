b7f351862ac06f207b744ee3996ea5c65db2d061
Jonathan Rincon <Jonathanandres080@gmail.com>
2022-08-13 21:34:13 -0500

    seccion 5

---
+++ seccion 5/tarea5.js
@@ -0,0 +1,12 @@
+// Una variable que contenga tu altura en centímetros (entero)
+let variable = 167;
+//Una variable que contenga tu altura en metros (número de coma flotante)
+let AlturaMetros = 1.67;
+//Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso = 67;
+//Una variable que contenga tu altura en metros redondeada hacia arriba
+let altura = AlturaMetros.toFixed(1);
+console.log(altura);
+//Una variable que contenga si "el máximo valor que se puede obtener en 
+//Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let max_valor_JS = Number.MAX_VALUE;
